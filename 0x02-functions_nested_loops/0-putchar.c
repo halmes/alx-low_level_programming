115d7e87e89abdd7d0ad4cecff9ced48524daec5
halmes <mesfinhale2@gmail.com>
2022-12-22 11:51:58 -0800

    Commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* * main - check the code
+* * Return: Always 0.
+* *
+*/
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+
+	{
+	putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* * print_alphabet - prints the english alphabet from a-z
+* * Return: Always 0.
+* *
+*/
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+	{
+	putchar(x);
+
+	}
+
+	putchar('\n');
+
+}
